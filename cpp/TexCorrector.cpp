8d045299be8bf41d7e649b082edcf1f2c52121fe
Michael <michailos34@mail.ru>
2023-09-10 11:04:37 +0300

    Упрощение скрипта

--- cpp/TexCorrector.cpp
+++ cpp/TexCorrector.cpp
@@ -4,13 +4,10 @@
 #include <map>
 #include <algorithm>
 
-std::string& makeStrTex(std::string& str, const std::map<std::string, std::string>& command, std::map<std::string, std::string> pathToPath)
+std::string& makeStrTex(std::string& str, const std::map<std::string, std::string>& command)
 {
     size_t pos = 0;
 
-    while ((pos = str.find("%", pos)) != std::string::npos)
-        str.replace(pos, 3, "");
-//std::cout << str << std::endl;
     for (const auto& c : command)
     {
         pos = 0;
@@ -23,18 +20,6 @@ std::string& makeStrTex(std::string& str, const std::map<std::string, std::strin
             pos += c.first.length();
         }
     }
-
-    pos = 0;
-//std::cout << str << std::endl;
-    for (const auto& c : pathToPath)
-    {
-        pos = 0;
-        while ((pos = str.find(c.first + ")", pos)) != std::string::npos)
-        {
-            str.replace(pos, c.first.length() + 1, c.second + ")");
-            pos += c.first.length();
-        }
-    }
     
     return str;
 }
@@ -42,74 +27,37 @@ std::string& makeStrTex(std::string& str, const std::map<std::string, std::strin
 int main (int argn, char* argv[])
 {
     std::string changeCommand = argv[1];
-    std::string output = argv[2];
     std::string md = argv[2];
-    std::string namePNG = argv[2];
-    std::string CorrectFilesPath = argv[3];
-    namePNG = namePNG.replace(namePNG.length() - 3, namePNG.length() - 1, "");
-
-    output = output.substr(0, output.length() - 2);
+    std::string output = argv[3];
 
     std::string str, correctStr;
 
-    std::ifstream changeCommandFile, mdFile, CorrectFilesPathFile;
+    std::ifstream changeCommandFile, mdFile;
     changeCommandFile.open(changeCommand, std::ios::in);
     mdFile.open(md, std::ios::in);
-    CorrectFilesPathFile.open(CorrectFilesPath, std::ios::in);
     std::ofstream outputFile;
     outputFile.open(output, std::ios::out);
 
+    std::cout << "Start: " << output << std::endl;
+
     std::map<std::string, std::string> command;
-    std::map<std::string, std::string> pathToPath;
 
     while (changeCommandFile >> str >> correctStr)
         command[str] = correctStr;
-
-    while (std::getline(CorrectFilesPathFile, str))
-    {
-        std::getline(CorrectFilesPathFile, correctStr);
-
-        size_t pos = 0;
-
-        if ((pos = correctStr.find(".md.e", pos)) != std::string::npos)
-            correctStr.replace(pos, 5, ".md");
-
-        for (size_t i{}; i < str.length(); ++i)
-        {
-            if (str[i] < 0 || str[i] == ' ')
-            {
-                str.replace(i, 1, "");
-                --i;
-            }
-        }
-
-        pathToPath[str] = correctStr;
-
-        pos = 0;
-
-        while ((pos = str.find('/', 0)) != std::string::npos)
-        {
-            str = str.substr(str.find('/', 0) + 1);
-            correctStr = correctStr.substr(correctStr.find('/', 0) + 1);
-
-            pathToPath[str] = correctStr;
-        }
-
-        //std::cout << str << " " << correctStr << "\n";
-    }
 
     str = "";
 
     while (getline(mdFile, str))
     {
-        makeStrTex(str, command, pathToPath);
+        makeStrTex(str, command);
 
         outputFile << str << std::endl;
     }
 
+    std::cout << "Finish: " << output << std::endl;
+
     changeCommandFile.close();
     mdFile.close();
-    CorrectFilesPathFile.close();
     outputFile.close();
 
     return 0;
